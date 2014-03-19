262cb784a5af68dc0a595c3c6ff1d1f5e775b359
Fellipe Brito <fellipe@ladodireito.com>
2014-03-19 04:34:21 +0000

    Rails conf added

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,19 +2,24 @@
 lock '3.1.0'
 
 set :application, '__YOUR_APP__'
-set :log_level, :debug
+set :log_level, :info #debug
+#set :rvm_ruby_version, '2.0.0-p451'
 
 set :repo_url, "__YOUR_GIT_REPO__"
 set :branch,   proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 set :deploy_to, "/var/www"
 
 # Aliases
-# set :linked_files, %w{config/database.yml}
+#set :linked_files, %w{config/database.yml vendor/database/config/settings.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Unicorn Defs
 set :unicorn_config_path, -> { File.join(current_path, "vendor", "deploy", "unicorn.rb") }
 set :unicorn_pid, "#{deploy_to}/shared/tmp/pids/unicorn.pid"
+
+# Rails Defs
+# set :rails_env, 'production'
+# set :unicorn_rack_env, 'production'
 
 after 'deploy:publishing', 'deploy:restart'
 
